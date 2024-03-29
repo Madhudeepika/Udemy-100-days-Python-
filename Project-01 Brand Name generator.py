0faef93726a0292d03b4df2504f00a779a0787bd
ORUGANTI AMSU MADHU DEEPIKA <126512211+Madhudeepika@users.noreply.github.com>
2024-03-29 18:02:47 +0530

    Add files via upload

---
+++ Project-01 Brand Name generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
+
+
+# In[ ]:
+
+
+
+
